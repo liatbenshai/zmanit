34d7c9520b958191af6dab5c04b238346fbced6b
liatbenshai <48071856+liatbenshai@users.noreply.github.com>
2026-01-07 08:52:36 +0200

    Delete src/DailyView directory

--- src/DailyView/index.js
+++
@@ -1,7 +0,0 @@
-export { default as DailyView } from './DailyView';
-export { default as DailyTaskCard } from './DailyTaskCard';
-export { default as SimpleTaskForm } from './SimpleTaskForm';
-export { default as WeeklyCalendarView } from './WeeklyCalendarView';
-export { default as SmartDayPlanner } from './SmartDayPlanner';
-export { default as FocusedDashboard } from './FocusedDashboard';
-export { TASK_TYPES } from './DailyView';
